eef821571a40e81decc77a77e3a4c50a854a2144
Jason Crescenzo <crescenzojay@gmail.com>
2018-11-01 16:34:54 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,6 +27,15 @@ def apply_discount
   if @discount > 0 
     @discount = @discount/100.f 
     @total = @total * (1- @discount)
+    "Success new total is #{total}"
+  end
+else 
+  "error message that there is no discount to apply"
+end 
 end
 
-# CashRegister #apply_discount the cash register was initialized with an employee discount applies the discount to the total price
+rspec ./spec/cash_register_spec.rb:43 # CashRegister #apply_discount the cash register was initialized with an employee discount applies the discount to the total price
+rspec ./spec/cash_register_spec.rb:49 # CashRegister #apply_discount the cash register was initialized with an employee discount returns success message with updated total
+rspec ./spec/cash_register_spec.rb:54 # CashRegister #apply_discount the cash register was initialized with an employee discount reduces the total
+rspec ./spec/cash_register_spec.rb:62 # CashRegister #apply_discount the cash register was not initialized with an employee discount returns a string error message that there is no discount to apply
+rspec ./spec/cash_register_spec.rb:78 # CashRegister #void_last_transaction subtracts the last transaction from the total
